0758aec24c32ca1f612e510a5a782ddf8500c6c6
Shraddha Gupta <75632406+shradhagupta12@users.noreply.github.com>
2025-06-29 15:17:19 +0000

    Datatype and Variable examples

---
+++ 01-Basics/01_variable.js
@@ -0,0 +1,18 @@
+
+
+const accountId = 1256723;
+let accountEmailId = 'shraddha@google.com';
+var accountName = 'Shraddha';
+accountAddress = 'Mumbai';
+
+// accountId = 12344221; 
+// const cannot be updated once value is declare and value need to assigned at the time of declartion
+
+console.log(accountId);
+
+console.table([accountId , accountEmailId , accountName , accountAddress]);
+
+/*
+Prfer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01-Basics/02_datatype.js
@@ -0,0 +1,29 @@
+let name = "Shraddha";
+let age = 18;
+let isLoggedIn = false;
+
+// Primitive Datatype
+/*
+    number
+    bigInt
+    string => ""
+    boolean => true/false
+    null => standalone
+    undefined => currently value is not assigned to the variable
+    symbol => unique
+*/
+
+// Non-Primitive Datatype
+
+/*
+    object
+    array
+    function
+*/
+
+// Example
+
+console.log(typeof null); // Object
+console.log(typeof undefined); // Undefined
+
+
