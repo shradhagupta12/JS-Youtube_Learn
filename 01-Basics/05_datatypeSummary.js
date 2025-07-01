2cc04a19e6107d6533b513446aa20cda751f190e
Shraddha Gupta <75632406+shradhagupta12@users.noreply.github.com>
2025-07-01 04:01:39 +0000

    Datatype Summary

---
+++ 01-Basics/05_datatypeSummary.js
@@ -0,0 +1,53 @@
+// Primitive Type (call by value)
+
+// 7 types : String , Number , Boolean , null , undefined , BigInt , Symbol
+
+// String : charaters and alphabet use in JS is called String.
+// Number : Number are the digit 
+// Boolean : True or False
+// null : Empty space 
+// undefined : variable is defined but value is not be assigned
+// BigInt : Number which are too big size some of the scientific numbers.
+// Symbol : To make any of the variable or function unique
+
+
+let studentId = 122389;
+let sscMarks = 23.05;
+
+let userName = "Shraddha";
+
+let isLoggedIn = false;
+
+let outsideTemp = null;
+
+let studentEmail;
+
+let bigNumber = 12943249328482374n
+
+let id = Symbol('1234');
+let anotherId = Symbol('1234');
+
+
+
+// Non-Primitive (call by refernce)
+
+// Array , Object , Functions
+
+const myArray = ["SpiderMan" , "IronMan" , "Hulk" , "BatMan"];
+
+let myObj = {
+    name : "Shraddha",
+    age : 12
+}
+
+let myFunction = function(){
+    console.log("My Function");
+}
+
+
+
+/*******************************************************************************/
+// Is Javascript a static type or dynamic type language ?
+/*******************************************************************************/
+
+
