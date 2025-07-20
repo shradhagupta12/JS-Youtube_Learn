d12433e304aac0bef30f9f6c2755c2c6c0ed44b2
Shraddha Gupta <75632406+shradhagupta12@users.noreply.github.com>
2025-07-20 15:17:46 +0000

    Condition and Loops

---
+++ 04-Control_Flow/one.js
@@ -0,0 +1,58 @@
+// If Loops
+
+let score = 80;
+
+/*
+if(score == 80){
+    console.log("Score is 80");
+}
+*/
+
+// If Else Loop
+
+/*
+if(score <= 80){
+    console.log(`Score is ${score}`);
+}else{
+    console.log(`Score is greater than ${score}`);  
+}
+*/
+
+// Nested If else loop
+
+/*
+if(score <= 20){
+    console.log("Score is greater than 20");    
+}else if(score <= 40){
+    console.log("Score is greater than 40");    
+}else if (score <= 80){
+    console.log("Score is greater than 80");    
+}else{
+    console.log(`Score is ${score}`);    
+}
+*/
+
+// Condtion if else loop
+
+let userLoggedIn = true;
+let debitCard = true;
+let googleLoggin = false;
+let emailLogin = true;
+
+/*
+if(userLoggedIn && debitCard){
+    console.log("Logged in the user and start shopping");    
+}else{
+    console.log("All the details are not avalable")
+}
+
+*/
+
+
+/*
+if(userLoggedIn && debitCard || googleLoggin || emailLogin){
+    console.log("Logged in successfully")
+}else{
+    console.log("Not logged in");
+}
+*/
---
+++ 04-Control_Flow/switch.js
@@ -0,0 +1,21 @@
+let month = 3;
+let monthWord = "march";
+
+
+switch (monthWord) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("Mar");
+        break;
+    case "april":
+        console.log("Apr");
+        break;
+    default:
+        console.log("Default Month");
+        break;
+}
