f3a1a8aa23cb3aa9ea7eeb79d10a3dec7778673c
Shraddha Gupta <75632406+shradhagupta12@users.noreply.github.com>
2025-07-22 10:28:02 +0000

    While and Do While loop

---
+++ 05-Iteration/two.js
@@ -0,0 +1,22 @@
+let index = 0;
+while (index <= 10) {
+    // console.log(index);
+    index = index + 2;
+}
+
+let myHero = ['flash' , 'Batman' , 'SuperMan' , 'SpiderMan' , 'IronMan']
+
+let arr = 0;
+
+while (arr < myHero.length) {
+    // console.log(myHero[arr]);
+    arr = arr + 1
+}
+
+// Do while loop
+let score = 11
+
+do {
+    console.log(score);
+    score ++
+} while (score <= 10);
