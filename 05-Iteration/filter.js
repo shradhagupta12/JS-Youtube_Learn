2bb9339844c3a38bc0fb431e9c213c14386a7360
Shraddha Gupta <75632406+shradhagupta12@users.noreply.github.com>
2025-07-27 05:47:54 +0000

    Filter method

---
+++ 05-Iteration/filter.js
@@ -0,0 +1,23 @@
+const myNumber = [1 , 2 , 3 , 4 , 5 , 6 ,7 , 8 , 9 , 10];
+
+// const newNum = myNumber.filter((num) => num > 4);
+// const newNum = myNumber.filter((num) => {
+//     return num > 4
+// });
+// console.log(newNum);
+
+
+// Using forEach Loop
+
+// const newNum = []
+// myNumber.forEach((num) => {
+//     if(num > 4){
+//         newNum.push(num)
+//     }
+// })
+
+// console.log(newNum);
+
+
+
+
