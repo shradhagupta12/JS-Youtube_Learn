202c79caf6039ee93836c470a7610bf26cbf395a
Shraddha Gupta <75632406+shradhagupta12@users.noreply.github.com>
2025-07-27 04:12:30 +0000

    For in loop for Array and Object

---
+++ 05-Iteration/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js : 'Javascript',
+    cpp : 'C++',
+    rb : 'Ruby',
+    html : 'Hypter Text Markup Language',
+    css : 'Cascading Style Sheet'
+}
+
+/*
+for (const key in myObject) {
+   console.log(myObject[key]); // Value of the object 
+}
+*/
+
+/*
+for(const short in myObject){
+    console.log(`${short} of the language ${myObject[short]}`);
+}
+*/
+
+const myLang = ['js' , 'cpp' , 'py' , 'java']
+
+/*
+for (const key in myLang) {
+  console.log(key); 
+  console.log(myLang[key]); 
+}
+
+*/
